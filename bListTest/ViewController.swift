16eab702394f0d0a293901d4f1b1c78ad0bc1718
Liseth Cardozo <cardozoliseth@gmail.com>
2017-09-21 13:24:24 -0700

    create a simple todo app

--- bListTest/ViewController.swift
+++ bListTest/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var userInputLabel: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var beatsList = ["study algos", "buy a dress"]
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func beastButtonWasPressed(_ sender: UIButton) {
+        print("Pressed")
+        let item = userInputLabel.text!
+        beatsList.append(item)
+        tableView.reloadData()
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return beatsList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+        cell?.textLabel?.text = beatsList[indexPath.row]
+        print("are u workign")
+        
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        beatsList.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
+    
+    
 }
 
